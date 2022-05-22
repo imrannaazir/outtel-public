d3b2991294d6bab04edd781bffd4bcbe6ea487b7
unknown <imran nazir>
2022-05-22 19:26:51 +0600

    counter section added

---
+++ src/pages/Home/Analysis.jsx
@@ -0,0 +1,62 @@
+import {
+  faComputer,
+  faFlagCheckered,
+  faMessage,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CountUp from "react-countup";
+import React from "react";
+
+const Analysis = () => {
+  const infos = [
+    {
+      id: 1,
+      name: "Countries",
+      logo: faFlagCheckered,
+      quantity: 165,
+    },
+    {
+      id: 2,
+      name: "Complete Projects",
+      logo: faComputer,
+      quantity: 540,
+    },
+    {
+      id: 3,
+      name: "Happy Clients",
+      logo: faUsers,
+      quantity: 630,
+    },
+    {
+      id: 1,
+      name: "FeedBacks",
+      logo: faMessage,
+      quantity: 550,
+    },
+  ];
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 m-6 ">
+      {infos.map((info) => (
+        <>
+          <div
+            className=" flex flex-col items-center gap-4 border m-2 py-6 hover:border-0 hover:shadow-xl uppercase"
+            key={info.id}
+          >
+            <FontAwesomeIcon
+              className="text-5xl text-gray-600"
+              icon={info.logo}
+            />
+            <span className="text-3xl text-primary">
+              <CountUp duration={5} end={info.quantity} />
+              <span>+</span>
+            </span>
+            <p className="text-xl text-black">{info.name}</p>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Analysis;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "../../Shared/Footer";
+import Analysis from "./Analysis";
 import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Analysis />
+      <Footer />
     </div>
   );
 };
