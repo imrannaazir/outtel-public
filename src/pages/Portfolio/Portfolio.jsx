83e5999e94afddc14e3824d02f8b3627e651a2be
unknown <imran nazir>
2022-05-23 20:49:15 +0600

    protfolio about me section added

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import MyDetails from "./MyDetails";
 import PortfolioBanner from "./PortfolioBanner";
 
 const Portfolio = () => {
   return (
     <div>
       <PortfolioBanner />
+      <MyDetails />
     </div>
   );
 };
