33890ab2a8af262c7cf75660fdcf9d8c1aa97fcd
unknown <imran nazir>
2022-05-24 23:46:13 +0600

    successfully fetched and loaded data from reverse

--- src/pages/Home/Part.jsx
+++ src/pages/Home/Part.jsx
@@ -2,16 +2,15 @@ import { faBagShopping, faWarehouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const Part = ({ i }) => {
+const Part = ({
+  i,
+  part: { image, quantity, min_quantity, name, description, price },
+}) => {
   return (
     <div class="hero bg-base-200 border-2">
       <div class="grid lg:grid-cols-2 justify-items-center my-10 w-[80%] mx-auto">
         <div className={i % 2 === 0 ? "order-1" : "order-2"}>
-          <img
-            src="https://api.lorem.space/image/movie?w=260&h=400"
-            class="max-w-sm rounded-lg shadow-2xl"
-            alt=""
-          />
+          <img src={image} class="max-w-sm rounded-lg shadow-2xl" alt="" />
         </div>
 
         <div
@@ -19,37 +18,23 @@ const Part = ({ i }) => {
             i % 2 === 0 ? "order-2 items-start" : "order-1 items-end text-right"
           } flex flex-col justify-center`}
         >
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <h1 class="text-5xl font-bold">{name}</h1>
           <p className="flex gap-6">
             <span>
-              <FontAwesomeIcon icon={faWarehouse} /> Available : 150
+              <FontAwesomeIcon icon={faWarehouse} /> Available : {quantity}
             </span>
             <span>
-              <FontAwesomeIcon icon={faBagShopping} /> Order minimum : 15
+              <FontAwesomeIcon icon={faBagShopping} /> Order minimum :{" "}
+              {min_quantity}
             </span>
           </p>
-          <p class="my-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <p class="my-6">{description}</p>
           <p className="">
-            <span className="text-5xl">$5</span>
+            <span className="text-5xl">${price}</span>
             <span>/1</span>
           </p>
-          <form class="form-control my-4">
-            <label class="input-group">
-              <span>Quantity</span>
-              <input
-                type="number"
-                min="150"
-                placeholder="150"
-                max="160"
-                class="input input-bordered"
-              />
-            </label>
-            <button class="btn btn-primary mt-4">purchase</button>
-          </form>
+
+          <button class="btn btn-primary mt-4">purchase</button>
         </div>
       </div>
     </div>
--- src/pages/Home/Parts.jsx
+++ src/pages/Home/Parts.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Part from "./Part";
 
 const Parts = () => {
-  const parts = ["1", "2", "3"];
+  const [parts, setParts] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const { data } = await axios.get("http://localhost:5000/parts");
+      setParts(data);
+    })();
+  }, []);
   return (
     <div id="parts">
       {parts.map((part, i) => (
-        <Part key={i} i={i} />
+        <Part key={i} i={i} part={part} />
       ))}
     </div>
   );
