5cf213d2628422fac41b20e765682e9ca1c4080d
emon <imrannaaziremon@gmail.com>
2022-09-22 09:49:38 +0600

    font added to heading

--- src/pages/Home/Heading.jsx
+++ src/pages/Home/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heading = ({ children }) => {
   return (
-    <div className="flex flex-col justify-center items-center my-6 space-y-4">
+    <div className="flex flex-col justify-center items-center my-6 space-y-4 font-[Poppins]">
       <p className="text-4xl uppercase text-center ">{children}</p>
       <div className="h-[2px] w-8  bg-primary rounded-full" />
     </div>
